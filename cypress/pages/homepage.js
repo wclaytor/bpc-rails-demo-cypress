963eb9fa1a3d53b076a4a6f0838ae51a08211508
Bill Claytor <bill.claytor@active911.com>
2023-03-08 14:55:24 -0700

    Updated selectors to use ID Updated npm scripts

--- cypress/pages/homepage.js
+++ cypress/pages/homepage.js
@@ -1,27 +1,27 @@
 class Homepage{
 
   elements = {
-    navbar : () => cy.xpath("//nav[@id='main-navbar']"),
-    navbarBrand : () => cy.xpath("//span[@class='navbar-brand'][text()='Developer Bookshelf']"),
-    navbarLinkHome : () => cy.xpath("//a[@class='nav-link'][text()='Home']"),
-    navbarLinkAuthors : () => cy.xpath("//a[@class='nav-link'][text()='Authors']"),
-    navbarLinkBooks : () => cy.xpath("//a[@class='nav-link'][text()='Books']"),
-    header : () => cy.xpath("//div[@id='header']"),
-    textHeading : () => cy.xpath("//h1[text()='Developer Bookshelf']"),
-    textSubHeading : () => cy.xpath("//h3[text()='A Ruby on Rails demo']"),
-    imgBackground : () => cy.xpath(""),
-    content : () => cy.xpath("//div[@id='content']"),
-    linkBrowseByAuthor : () => cy.xpath("//a[text()='author name']"),
-    linkBrowseByBook : () => cy.xpath("//a[text()='book title']"),
-    linkGithub : () => cy.xpath("//a[text()='Github']"),
-    linkKanbanBoard : () => cy.xpath("//a[text()='Kanban board']"),
-    footer : () => cy.xpath("//div[@id='footer']"),
-    linkPhotoCredit : () => cy.xpath("//a[text()='Photo by Engin Akyurt']"),
+    navbar : () => cy.get("nav[id='navbar']"),
+    navbarBrand : () => cy.get("span[id='navbarBrand']"),
+    navbarLinkHome : () => cy.get("a[id='navbarLinkHome']"),
+    navbarLinkAuthors : () => cy.get("a[id='navbarLinkAuthors']"),
+    navbarLinkBooks : () => cy.get("a[id='navbarLinkBooks']"),
+    jumbotron : () => cy.get("div[id='jumbotron']"),
+    textJumbotronHeading : () => cy.get("h1[id='textJumbotronHeading']"),
+    textJumbotronSubHeading : () => cy.get("h3[id='textJumbotronSubHeading']"),
+    imgBackground : () => cy.get(""),
+    content : () => cy.get("div[id='content']"),
+    linkBrowseByAuthor : () => cy.get("a[id='browseAuthors']"),
+    linkBrowseByBook : () => cy.get("a[id='browseBooks']"),
+    linkGithub : () => cy.get("a[id='github']"),
+    linkKanbanBoard : () => cy.get("a[id='kanban']"),
+    footer : () => cy.get("div[id='footer']"),
+    linkPhotoCredit : () => cy.get("a[id='photoCredit']"),
   }
 
   verifyPageElements() {
     this.verifyNavbarElements();
-    this.verifyHeaderElements();
+    this.verifyJumbotronElements();
     this.verifyContentElements();
     this.verifyFooterElements();
   }
@@ -34,27 +34,39 @@ class Homepage{
     this.elements.navbarLinkBooks().should("be.visible");
   }
 
-  verifyHeaderElements() {
-    this.elements.header().should("be.visible");
-    this.elements.textHeading().should("be.visible");
-    this.elements.textSubHeading().should("be.visible");
+  verifyJumbotronElements() {
+    this.elements.jumbotron().should("be.visible");
+    this.elements.textJumbotronHeading()
+      .should("be.visible")
+      .and('have.text', 'Developer Bookshelf');
+    this.elements.textJumbotronSubHeading()
+      .should("be.visible")
+      .and('have.text', 'A Ruby on Rails demo');
     // this.elements.imgBackground.should("be.visible");
   }
 
   verifyContentElements() {
     this.elements.content().should("be.visible");
-    this.elements.linkBrowseByAuthor().should("be.visible");
-    this.elements.linkBrowseByBook().should("be.visible");
-    this.elements.linkGithub().should("be.visible");
-    this.elements.linkKanbanBoard().should("be.visible");
+    this.elements.linkBrowseByAuthor()
+      .should("be.visible")
+      .and('have.text', 'author name');
+    this.elements.linkBrowseByBook()
+      .should("be.visible")
+      .and('have.text', 'book title');
+    this.elements.linkGithub()
+      .should("be.visible")
+      .and('have.text', 'Github');
+    this.elements.linkKanbanBoard()
+      .should("be.visible")
+      .and('have.text', 'Kanban board');
   }
 
   verifyFooterElements() {
     this.elements.footer().should("be.visible");
-    this.elements.linkPhotoCredit().should("be.visible");
+    this.elements.linkPhotoCredit()
+      .should("be.visible")
+      .and('have.text', 'Photo by Engin Akyurt');
   }
-
-
 
 }
 
